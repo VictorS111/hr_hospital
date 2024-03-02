a3a766da2d937728ac54431e2e4c9afa99178042
viktors111 <viktor2520072@gmail.com>
2024-03-02 21:54:56 +0200

    model patient

---
+++ hr_hospital1/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Viktor',
+    'website': 'www.odoo.tech',
+    'summary': 'Odoo 17 Development',
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
