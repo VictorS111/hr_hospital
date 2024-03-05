fed5a078812e3ccef21fc498ed729cdd038f1e0f
viktors111 <viktor2520072@gmail.com>
2024-03-05 02:27:19 +0200

    Added chatter, compute fields, sequential value, search view

--- hr_hospital1/__manifest__.py
+++ hr_hospital1/__manifest__.py
@@ -3,8 +3,10 @@
     'author': 'Viktor',
     'website': 'www.odoo.tech',
     'summary': 'Odoo 17 Development',
+    'depends': ['mail'],
     'data': [
         # 'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/menu.xml',
         'views/patient.xml',
     ]
